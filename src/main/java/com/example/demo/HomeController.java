8bf82b8240f536fddfedd5797bdae88feaa1922e
GBTC440005UR <GBTC440005UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-08-01 15:57:31 -0400

    Ver 1.0

---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,99 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
+
+@Controller
+public class HomeController {
+    @Autowired
+    OrderRepository orderRepository;
+
+    @RequestMapping("/")
+    public String homePage(Model model) {
+        model.addAttribute("pizzaorders", orderRepository.findAll());
+        return "home";
+    }
+
+    @GetMapping("/add")
+    public String orderForm(Model model) {
+        model.addAttribute("pizzaorder", new PizzaOrder());
+        return "orderform";
+    }
+
+    @PostMapping("/process")
+    public String processOrderForm(@Valid PizzaOrder pizzaOrder) {
+//        if (result.hasErrors()) {
+//            return "orderform";
+//        }
+        if (pizzaOrder.isGlutenFreeDough()) {
+            pizzaOrder.setPrice(pizzaOrder.addUpTotal());
+        }
+        if (pizzaOrder.isNormalDough()) {
+            pizzaOrder.setPrice(pizzaOrder.addUpTotal());
+        }
+        if (pizzaOrder.isRedSauce()) {
+            pizzaOrder.setPrice(pizzaOrder.addUpTotal());
+        }
+        if (pizzaOrder.isWhiteSauce()) {
+            pizzaOrder.setPrice(pizzaOrder.addUpTotal());
+        }
+        if (pizzaOrder.isCheese()) {
+            pizzaOrder.setPrice(pizzaOrder.addUpTotal());
+        }
+        if (pizzaOrder.isMushroom()) {
+            pizzaOrder.setPrice(pizzaOrder.addUpTotal());
+        }
+        if (pizzaOrder.isOnions()) {
+            pizzaOrder.setPrice(pizzaOrder.addUpTotal());
+        }
+        if (pizzaOrder.isGreenPepper()) {
+            pizzaOrder.setPrice(pizzaOrder.addUpTotal());
+        }
+        if (pizzaOrder.isBacon()) {
+            pizzaOrder.setPrice(pizzaOrder.addUpTotal());
+        }
+        if (pizzaOrder.isPepperoni()) {
+            pizzaOrder.setPrice(pizzaOrder.addUpTotal());
+        }
+        if (pizzaOrder.isSausage()) {
+            pizzaOrder.setPrice(pizzaOrder.addUpTotal());
+        }
+        if (pizzaOrder.isSmallSize()) {
+            pizzaOrder.setPrice(pizzaOrder.addUpTotal());
+        }
+        if (pizzaOrder.isMediumSize()) {
+            pizzaOrder.setPrice(pizzaOrder.addUpTotal() + 1);
+        }
+        if (pizzaOrder.isLargeSize()) {
+            pizzaOrder.setPrice(pizzaOrder.addUpTotal() + 2);
+        }
+
+        orderRepository.save(pizzaOrder);
+        return "redirect:/";
+    }
+
+    @RequestMapping("/detail/{id}")
+    public String showDetailPizza(@PathVariable("id") long id, Model model) {
+        model.addAttribute("pizzaorder", orderRepository.findById(id).get());
+        return "show";
+    }
+
+    @RequestMapping("/update/{id}")
+    public String updateMessage(@PathVariable("id") long id, Model model) {
+        model.addAttribute("pizzaorder", orderRepository.findById(id).get());
+        return "updateorderform";
+    }
+
+    @RequestMapping("/delete/{id}")
+    public String delMessage(@PathVariable("id") long id) {
+        orderRepository.deleteById(id);
+        return "redirect:/";
+    }
+}
---
+++ src/main/java/com/example/demo/PizzaOrder.java
@@ -0,0 +1,227 @@
+package com.example.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Calendar;
+
+@Entity
+public class PizzaOrder {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private boolean smallSize;
+
+    private boolean mediumSize;
+
+    private boolean largeSize;
+
+    private boolean normalDough;
+
+    private boolean glutenFreeDough;
+
+    private boolean redSauce;
+
+    private boolean whiteSauce;
+
+    private boolean noSauce;
+
+    private double price;
+
+    private boolean cheese;
+
+    private boolean noCheese;
+
+    private boolean mushroom;
+
+    private boolean onions;
+
+    private boolean greenPepper;
+
+    private boolean bacon;
+
+    private boolean pepperoni;
+
+    private boolean sausage;
+
+    private String time;
+
+    private Calendar calendar;
+
+    public PizzaOrder() {
+        calendar = Calendar.getInstance();
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public boolean isNormalDough() {
+        return normalDough;
+    }
+
+    public void setNormalDough(boolean normalDough) {
+        this.normalDough = normalDough;
+    }
+
+    public boolean isGlutenFreeDough() {
+        return glutenFreeDough;
+    }
+
+    public void setGlutenFreeDough(boolean glutenFreeDough) {
+        this.glutenFreeDough = glutenFreeDough;
+    }
+
+    public boolean isRedSauce() {
+        return redSauce;
+    }
+
+    public void setRedSauce(boolean redSauce) {
+        this.redSauce = redSauce;
+    }
+
+    public boolean isWhiteSauce() {
+        return whiteSauce;
+    }
+
+    public void setWhiteSauce(boolean whiteSauce) {
+        this.whiteSauce = whiteSauce;
+    }
+
+    public boolean isCheese() {
+        return cheese;
+    }
+
+    public void setCheese(boolean cheese) {
+        this.cheese = cheese;
+    }
+
+    public boolean isMushroom() {
+        return mushroom;
+    }
+
+    public void setMushroom(boolean mushroom) {
+        this.mushroom = mushroom;
+    }
+
+    public boolean isOnions() {
+        return onions;
+    }
+
+    public void setOnions(boolean onions) {
+        this.onions = onions;
+    }
+
+    public boolean isGreenPepper() {
+        return greenPepper;
+    }
+
+    public void setGreenPepper(boolean greenPepper) {
+        this.greenPepper = greenPepper;
+    }
+
+    public boolean isBacon() {
+        return bacon;
+    }
+
+    public void setBacon(boolean bacon) {
+        this.bacon = bacon;
+    }
+
+    public boolean isPepperoni() {
+        return pepperoni;
+    }
+
+    public void setPepperoni(boolean pepperoni) {
+        this.pepperoni = pepperoni;
+    }
+
+    public boolean isSausage() {
+        return sausage;
+    }
+
+    public void setSausage(boolean sausage) {
+        this.sausage = sausage;
+    }
+
+    public boolean isNoCheese() {
+        return noCheese;
+    }
+
+    public void setNoCheese(boolean noCheese) {
+        this.noCheese = noCheese;
+    }
+
+    public boolean isSmallSize() {
+        return smallSize;
+    }
+
+    public void setSmallSize(boolean smallSize) {
+        this.smallSize = smallSize;
+    }
+
+    public boolean isMediumSize() {
+        return mediumSize;
+    }
+
+    public void setMediumSize(boolean mediumSize) {
+        this.mediumSize = mediumSize;
+    }
+
+    public boolean isLargeSize() {
+        return largeSize;
+    }
+
+    public void setLargeSize(boolean largeSize) {
+        this.largeSize = largeSize;
+    }
+
+    public boolean isNoSauce() {
+        return noSauce;
+    }
+
+    public void setNoSauce(boolean noSauce) {
+        this.noSauce = noSauce;
+    }
+
+    public double getPrice() {
+
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        calendar.add(Calendar.MINUTE, 50);
+        time = String.valueOf(calendar.getTime());
+        this.time = time;
+    }
+
+    public Calendar getCalendar() {
+        return calendar;
+    }
+
+    public void setCalendar(Calendar calendar) {
+        this.calendar = calendar;
+    }
+
+    public double addUpTotal() {
+
+        return getPrice() + 1;
+    }
+
+
+}
